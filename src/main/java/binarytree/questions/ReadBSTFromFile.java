9dcfa074de3eccc24e6d15d979fcbd5beaa7eedb
shoubhik <shoubhikbhatti@gmail.com>
2013-02-03 22:29:50 -0500

    more tree questions

--- src/main/java/binarytree/questions/ReadBSTFromFile.java
+++ src/main/java/binarytree/questions/ReadBSTFromFile.java
@@ -84,10 +84,9 @@ public class ReadBSTFromFile implements BinaryTreeConstructor {
 
         this.in = new BufferedReader(fstream);
         // empty binary terr
-        this.binaryTree = new BinaryTree();
+
     }
 
-    @Override
     /**
      * Remember my post: Determine if a Binary Tree is a Binary Search Tree
      * (BST) http://www.leetcode.com/2010/09/determine-if-binary-tree-is-binary.html?
@@ -100,6 +99,7 @@ public class ReadBSTFromFile implements BinaryTreeConstructor {
      will take only O(n) time.
      */
     public BinaryTree construct() {
+        this.binaryTree = new BinaryTree();
         try {
             // int data[] = getDataFromFile();
             int data = Integer.parseInt(this.in.readLine());
